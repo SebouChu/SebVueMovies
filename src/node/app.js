d6eb61708cdd87fe036ed43cc8b6831d00e2b002
Sébastien Gaya <sebastien.gaya@gmail.com>
2019-02-15 19:50:59 +0100

    removed global movies ; now use mongodb

--- src/node/app.js
+++ src/node/app.js
@@ -21,51 +21,6 @@ MongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, function
     global.DB_CLIENT = client;
 });
 
-global.MOVIES = [
-    {
-        id: 1,
-        title: "Fantastic Beasts: The Crimes of Grindelwald",
-        year: 2018,
-        language: 'Anglais',
-        director: {
-            name: "David Yates",
-            nationality: "Britannique",
-            birthdate: "1963-11-30"
-        },
-        poster: null,
-        genre: 'Fantastique',
-        ratings: [4, 3, 3]
-    },
-    {
-        id: 2,
-        title: "Titanic",
-        year: 1997,
-        language: 'Anglais',
-        director: {
-            name: "James Cameron",
-            nationality: "Canadien",
-            birthdate: "1954-08-16"
-        },
-        poster: null,
-        genre: 'Historique',
-        ratings: [5, 4]
-    },
-    {
-        id: 3,
-        title: "Interstellar",
-        year: 2014,
-        language: 'Anglais',
-        director: {
-            name: "Christopher Nolan",
-            nationality: "Britannique",
-            birthdate: "1970-07-30"
-        },
-        poster: null,
-        genre: 'Science-fiction',
-        ratings: [5, 5, 5, 5]
-    }
-];
-
 var app = express();
 
 app.use(express.static(path.resolve('src/static')));
