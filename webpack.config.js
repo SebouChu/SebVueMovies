c7d42c36758f306da3ac67f7f16d3f67afc23025
Sébastien Gaya <sebastien.gaya@gmail.com>
2019-05-12 16:55:52 +0200

    Production Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -63,14 +63,10 @@ module.exports = {
 }
 
 if (process.env.NODE_ENV === 'production') {
+    module.exports.mode = 'production'
     module.exports.devtool = '#source-map'
     // http://vue-loader.vuejs.org/en/workflow/production.html
     module.exports.plugins = (module.exports.plugins || []).concat([
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: '"production"'
-            }
-        }),
         new webpack.LoaderOptionsPlugin({
             minimize: true
         })
